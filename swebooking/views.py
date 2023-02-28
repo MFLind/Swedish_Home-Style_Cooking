c1eea94c4cc7a9ba8b49da4c1b7b3d7792bd086e
MFLind <Madde@quaggatech.com>
2023-02-28 08:20:58 +0000

    Convert menu for template

--- swebooking/views.py
+++ swebooking/views.py
@@ -4,4 +4,8 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'swebooking/index.html')
 
+def menu(request):
+    return render(request, 'swebooking/menu.html')
+
+
 
