843ed6683372e86b38ee8f7c6a620815bbc70a8c
MFLind <Madde@quaggatech.com>
2023-02-28 09:39:01 +0000

    work in progress on booking

--- swebooking/urls.py
+++ swebooking/urls.py
@@ -5,4 +5,5 @@ from swebooking import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('menu/', views.menu, name='menu'),
+    path('booking/', views.booking, name='booking'),
 ]
--- swebooking/views.py
+++ swebooking/views.py
@@ -4,8 +4,11 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'swebooking/index.html')
 
+
 def menu(request):
     return render(request, 'swebooking/menu.html')
 
 
+def booking(request):
+    return render(request, 'swebooking/booking.html')
 
