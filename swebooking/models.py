d0fd7d5d26cb6908bae0bcc4a0c6a31ee91ccbd0
MFLind <Madde@quaggatech.com>
2023-02-28 08:06:18 +0000

    Make swebooking models and structure

--- swebooking/models.py
+++ swebooking/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class TableBooking(models.Model):
+
+    name = models.CharField(max_length=48)
+    booking_date_time = models.DateTimeField('Booking time and date')
+    persons = models.PositiveIntegerField()
+    telephone_number = models.CharField(max_length=16)
+    booking_created = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f' Account {self.user} did a booking for guest {self.name}, {self.persons} Persons '  \
+            f' at {self.booking_date_time}'
