787e46230d4d6274e7220f016f692f0409117e64
MFLind <Madde@quaggatech.com>
2023-03-01 09:41:24 +0000

    Change to use bootstrap

--- swebooking/forms.py
+++ swebooking/forms.py
@@ -1,33 +1,13 @@
 from django import forms
 from .models import TableBooking
+from tempus_dominus.widgets import DateTimePicker
+from django.forms.widgets import NumberInput  
 
 
 class BookingForm(forms.ModelForm):
-
-    booking_date_time = forms.DateTimeField(
-        widget=DateTimePicker(
-            options={
-                'useCurrent': True,
-                'collapse': False,
-
-                # Calendar and time widget formatting
-                'time': 'fa fa-clock-o',
-                'date': 'fa fa-calendar',
-                'up': 'fa fa-arrow-up',
-                'down': 'fa fa-arrow-down',
-                'previous': 'fa fa-chevron-left',
-                'next': 'fa fa-chevron-right',
-                'today': 'fa fa-calendar-check-o',
-                'clear': 'fa fa-delete',
-                'close': 'fa fa-times'
-            },
-            attrs={
-                'append': 'fa fa-calendar',
-                'icon_toggle': True,
-            }
-        )
-    )
+#    start_datetime=forms.SplitDateTimeField(input_time_formats=['%I:%M %p'])
 
     class Meta:
         model = TableBooking
         fields = ('name', 'telephone_number', 'persons', 'booking_date_time')
+
--- swebooking/models.py
+++ swebooking/models.py
@@ -10,7 +10,7 @@ class TableBooking(models.Model):
     telephone_number = models.CharField(max_length=16)
     booking_created = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return f' Account {self.user} did a booking for guest {self.name}, {self.persons} Persons '  \
