d4289260fdfa0e6bb9663c4ab9ccd1897dcb63f5
MFLind <Madde@quaggatech.com>
2023-03-01 10:04:00 +0000

    Added initial edit and delete of booking

--- swebooking/urls.py
+++ swebooking/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('menu/', views.menu, name='menu'),
     path('booking/', views.booking, name='booking'),
     path('seebookings/', views.seebookings, name='seebookings'),
+    path('edit/<id>', views.edit, name='edit'),
+    path('delete/<id>', views.delete, name='delete'),
 ]
--- swebooking/views.py
+++ swebooking/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
 from .forms import BookingForm
 from .models import TableBooking
 
@@ -30,3 +31,39 @@ def booking(request):
 
     return render(request, 'swebooking/booking.html', {'form': form})
 
+
+def edit(request, id):
+
+    tablebooking = get_object_or_404(TableBooking, id=id)
+
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        
+        if form.is_valid():
+            tablebooking.user = request.user
+            tablebooking.persons = form.cleaned_data['persons']
+            tablebooking.booking_date_time = form.cleaned_data['booking_date_time']
+            tablebooking.telephone_number = form.cleaned_data['telephone_number']
+            tablebooking.save()
+            return redirect('seebookings')
+
+    form = BookingForm(instance=tablebooking)
+
+    context = { 'booking': tablebooking, 'form': form}
+
+    return render(request, 'swebooking/editbooking.html', context)
+
+
+def delete(request, id):
+    tablebooking = get_object_or_404(TableBooking, id=id)
+
+    if request.method == 'POST':
+        
+        if tablebooking.delete():
+            return redirect('seebookings')
+
+    form = BookingForm(instance=tablebooking)
+
+    context = { 'booking': tablebooking, 'form': form}
+
+    return render(request, 'swebooking/delbooking.html', context)
