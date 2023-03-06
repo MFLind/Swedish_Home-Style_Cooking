52c3f25fc2f7db645a165768a7e4f52a70b4ae23
MFLind <Madde@quaggatech.com>
2023-03-06 08:35:49 +0000

    change BASE_DIR and Static

--- SwedishHomeStyleCooking/settings.py
+++ SwedishHomeStyleCooking/settings.py
@@ -21,7 +21,7 @@ if not os.environ.get("PRODUCTION"):
     load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -145,7 +145,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
